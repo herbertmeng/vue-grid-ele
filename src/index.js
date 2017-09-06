14fb4dfa3f2620cff1e60e92772cbfa58cdf20dd
ljw <leijiawang@analysys.com.cn>
2017-09-06 18:03:49 +0800

    update README

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import './packages/theme-default/lib/dropdown-menu.css'
 import './packages/theme-default/lib/dropdown.css'
 import './packages/theme-default/lib/tag.css'
 import './packages/theme-default/lib/tooltip.css'
-
+import vTable from './packages/table'
+import vTableColumn from './packages/table-column'
 import table from './table'
+table.table = vTable
+table.tableColumn = vTableColumn
 export default table
